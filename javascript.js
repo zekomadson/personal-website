5ef9ce0402fac3370568002628a486d485497a6a
Zeko_Madson <zdmadson@gmail.com>
2023-02-13 21:58:20 -0500

    Change margin and padding on press me buttons
    Update the 2 h2 elements in each sub-section that contain a 'press me' text to remove the margin and padding. There was too much space

--- javascript.js
+++ javascript.js
@@ -5,11 +5,25 @@ toggleContent= document.getElementById("sections-details")
 toggleButton.addEventListener("click",function(){
   toggleContent.style.display = toggleContent.style.display === "none" ? "flex" : "none";
 });
-*/ 
-const buttons = document.querySelectorAll(".section-header");
+
+const buttons = document.querySelectorAll(".toggle-button");
 buttons.forEach(button => {
   button.addEventListener("click", event => {
     const p = event.target.nextElementSibling;
     p.style.display = p.style.display === "none" ? "flex" : "none";
   });
 });
+*/ 
+const toggles = document.querySelectorAll(".toggle-button");
+var nextDiv = toggles.nextElementSibling;
+var toggleSigns = nextDiv.querySelectorAll("h2");
+toggles.addEventListener("click", function(){
+    for (var i = 0; i < toggleSigns.length; i++){
+        if (toggleSigns[i].style.display === "none") {
+            toggleSigns[i].style.display = "flex";
+          } else {
+            toggleSigns[i].style.display = "none";
+          }
+        }
+    });
+
