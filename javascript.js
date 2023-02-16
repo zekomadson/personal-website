2b73c1a38230e9d719776d6c44e66a8ae29e8ea3
Zeko_Madson <zdmadson@gmail.com>
2023-02-15 21:32:13 -0500

    Remove unnecessary comments in javascript code

--- javascript.js
+++ javascript.js
@@ -1,19 +1,4 @@
-/** 
-toggleButton = document.getElementsByClassName("section-header")
-toggleContent= document.getElementById("sections-details")
 
-toggleButton.addEventListener("click",function(){
-  toggleContent.style.display = toggleContent.style.display === "none" ? "flex" : "none";
-});
-
-const buttons = document.querySelectorAll(".toggle-button");
-buttons.forEach(button => {
-  button.addEventListener("click", event => {
-    const p = event.target.nextElementSibling;
-    p.style.display = p.style.display === "none" ? "flex" : "none";
-  });
-});
-*/ 
 const toggles = document.querySelectorAll(".toggle-button");
 var nextDiv = toggles.nextElementSibling;
 var toggleSigns = nextDiv.querySelectorAll("h2");
