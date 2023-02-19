3d88ce1ffb2f531d2244a8b373656f6ea8d373c4
Zeko_Madson <zdmadson@gmail.com>
2023-02-19 17:40:37 -0500

    Add jQuery library to enable toggle ability
    Implemented toggling of the subsections with jQuery script.

--- javascript.js
+++ javascript.js
@@ -1,31 +1,3 @@
-/*
-
-
-  <h2 id="toggleBtn" >Click me</h2>
-  <div id="myDiv">
-    <p>This content will be toggled</p>
-  </div>
-
-
-#myDiv{
-      display: none;
-    }
-.toggle-content{
-  display: none;
-}
-* {
-  border: 2px solid black;
-  padding: 5px;
-  margin: 5px;
-}
-
-
-
-*/
-
-const toggleBtn = document.querySelector('#toggleBtn');
-const myDiv = toggleBtn.nextElementSibling;
-toggleBtn.addEventListener('click', () => {
-  myDiv.style.display = myDiv.style.display === 'none' ? 'flex' : 'none';
+$(".toggle-button").click(function(){
+  $(this).next("div").toggleClass("hide-content");
 });
-
