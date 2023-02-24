8ba45d8c1a11771f1639734222f5079592592fad
Zeko_Madson <zdmadson@gmail.com>
2023-02-23 19:47:59 -0500

    Add boxicon library to javascript

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
+import 'boxicons'
+
+
 $(document).ready(function(){
     $(".toggle-button").click(function(){
         $(this).next("div").toggleClass("hide-content");
     });
 });
-
-/*
-
-<script>
-                $(".toggle-button").click(function(){
-                    $(this).next("div").toggleClass("hide-content");
-                });
-            </script>
-
-            */ 
