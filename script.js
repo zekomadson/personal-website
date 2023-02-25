7dd47905e3bb6bdcd9f4a42d2f84b054abf52bd6
Zeko_Madson <zdmadson@gmail.com>
2023-02-25 12:32:33 -0500

    Update front end file organization
    Removed and generated a few files. Replacement of the index.html file with a desktop.html and mobile.html files.

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ $(document).ready(function(){
         $(this).next("div").toggleClass("hide-content");
     });
 });
+
+/*
+// Get the screen width
+var screenWidth = window.innerWidth;
+
+// Check the screen width and load the appropriate HTML file
+if (screenWidth < 768) {
+  // Load the mobile HTML file
+  document.location.href = "mobile.html";
+} else {
+  // Load the desktop HTML file
+  document.location.href = "desktop.html";
+}
+*/
