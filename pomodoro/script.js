3a8443ea0b8f223bf6e5ce62ebf525d7f99b644e
Zeko_Madson <zdmadson@gmail.com>
2023-02-28 20:14:31 -0500

    Countdown now goes by minutes and seconds
    Update of the countdown to go by minutes and seconds. Still need to clean up the code as it looks to be quite messy

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,23 +1,27 @@
 
-        const startBtn = document.getElementById('start-btn');
-        const countdownEl = document.getElementById('countdown');
-        
-        startBtn.addEventListener('click', () => {
-          const numInput = document.getElementById('study-input');
-          let count = parseInt(numInput.value);
-          
-          if (isNaN(count) || count < 0 || count > 120) {
-            alert('Please enter a valid number between 0 and 100.');
-            return;
-          }
-          
-          countdownEl.innerHTML = count;
-          const countdownInterval = setInterval(() => {
-            count--;
-            countdownEl.innerHTML = count;
-            if (count === 0) {
-              clearInterval(countdownInterval);
-              countdownEl.innerHTML = 'Countdown Complete!';
-            }
-          }, 1000);
-        });
+ const startBtn = document.getElementById('start-btn');
+ const countdownEl = document.getElementById('countdown');
+ 
+ startBtn.addEventListener('click', () => {
+   const numInput = document.getElementById('study-input');
+   let totalSeconds = parseInt(numInput.value) * 60;
+   
+   if (isNaN(totalSeconds) || totalSeconds < 60 || totalSeconds > 3600) {
+     alert('Please enter a valid number between 1 and 60.');
+     return;
+   }
+   
+   const countdownInterval = setInterval(() => {
+     let minutes = Math.floor(totalSeconds / 60);
+     let seconds = totalSeconds % 60;
+     
+     countdownEl.innerHTML = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+     
+     if (totalSeconds === 0) {
+       clearInterval(countdownInterval);
+       countdownEl.innerHTML = 'Countdown Complete!';
+     }
+     
+     totalSeconds--;
+   }, 1000);
+ });
