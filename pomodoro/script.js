b4cbecd2d22c0a7677ab4d804aa730ddc6c54c87
Zeko_Madson <zdmadson@gmail.com>
2023-03-02 11:22:06 -0500

    Add clock div when button start button is pressed
    Implement jQuery code to add a 'div' element to the dom when the begin clock button is pressed. if the clock has already started, when the button is pressed it will print out 'pause clock' this is a placeholder for when the code to pausing the countdown is implemented

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,27 +1,17 @@
-
- const startBtn = document.getElementById('start-btn');
- const countdownEl = document.getElementById('countdown');
- 
- startBtn.addEventListener('click', () => {
-   const numInput = document.getElementById('study-input');
-   let totalSeconds = parseInt(numInput.value) * 60;
-   
-   if (isNaN(totalSeconds) || totalSeconds < 60 || totalSeconds > 3600) {
-     alert('Please enter a valid number between 1 and 60.');
-     return;
+$(document).ready(function(){
+    $("#start-btn").click(function(){ // Add clock to page if it doesn't exist or pause the clock if it does. 
+        if( $('#pomodoro-clock').length ) { 
+            console.log("Clock already generated. Pausing Clock")
+      } else { 
+        var pomodoroClock = $('div')
+        var clockDiv = $('.clock')
+        pomodoroClock.attr('id', 'pomodoro-clock')
+        pomodoroClock.append('Clock Image')
+        pomodoroClock.addClass('subFlex centerText')
+        clockDiv.addClass('columnFlex')
+        clockDiv.append(clock)
+      }
    }
-   
-   const countdownInterval = setInterval(() => {
-     let minutes = Math.floor(totalSeconds / 60);
-     let seconds = totalSeconds % 60;
-     
-     countdownEl.innerHTML = `${minutes}:${seconds.toString().padStart(2, '0')}`;
-     
-     if (totalSeconds === 0) {
-       clearInterval(countdownInterval);
-       countdownEl.innerHTML = 'Countdown Complete!';
-     }
-     
-     totalSeconds--;
-   }, 1000);
- });
+   );
+});
+
