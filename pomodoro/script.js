6070d6b9a94e7ce2618972b0ea3b297a7e450751
Zeko_Madson <zdmadson@gmail.com>
2023-03-03 09:52:09 -0500

    Add updated jQuery clock script

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,17 +1,26 @@
-$(document).ready(function(){
-    $("#start-btn").click(function(){ // Add clock to page if it doesn't exist or pause the clock if it does. 
-        if( $('#pomodoro-clock').length ) { 
-            console.log("Clock already generated. Pausing Clock")
-      } else { 
-        var pomodoroClock = $('div')
-        var clockDiv = $('.clock')
-        pomodoroClock.attr('id', 'pomodoro-clock')
-        pomodoroClock.append('Clock Image')
-        pomodoroClock.addClass('subFlex centerText')
-        clockDiv.addClass('columnFlex')
-        clockDiv.append(clock)
-      }
-   }
-   );
-});
+$("#start-btn").click(function(){ 
+    if( $('#pomodoro-clock').length ) {
+            var pc = $('#pomodoro-clock')
+                  if(pc.hasClass("running")){
+            pc.removeClass("running")
+          console.log("pause")
+        } else {
+            pc.addClass("running")
+          console.log("run again")
+        }
+    } else { 
+        console.log("here1")
+      $(".clock").append($("<p>Clock Running!</p>")).addClass('centerText running').attr('id', 'pomodoro-clock');
+    }
+ }
+);
+ 
+ 
+ 
+ /**
+ When you click on it:
+ - Check if it's the first time:
+     - if it is, add the p element
+  - if it is not, toggle the display between "Clock is running", "Clock is paused"
 
+ **/
