8a6e86f1f46c7ac8f81f6b80c29c65b82ba3e54e
Zeko_Madson <zdmadson@gmail.com>
2023-03-04 12:43:57 -0500

    Change Text of Clock Button on click
    update the button so that when it is click the text of the button switches from 'Pause' to 'Resume' and vice versa

--- pomodoro/script.js
+++ pomodoro/script.js
@@ -1,26 +1,10 @@
-$("#start-btn").click(function(){ 
-    if( $('#pomodoro-clock').length ) {
-            var pc = $('#pomodoro-clock')
-                  if(pc.hasClass("running")){
-            pc.removeClass("running")
-          console.log("pause")
-        } else {
-            pc.addClass("running")
-          console.log("run again")
-        }
-    } else { 
-        console.log("here1")
-      $(".clock").append($("<p>Clock Running!</p>")).addClass('centerText running').attr('id', 'pomodoro-clock');
+$(document).ready(function(){
+    $("#start-btn").click(function(){ 
+        if($(this).hasClass('run')){
+        $(this).text('Pause Clock')
+    } else {
+        $(this).text('Resume Clock')
     }
- }
-);
- 
- 
- 
- /**
- When you click on it:
- - Check if it's the first time:
-     - if it is, add the p element
-  - if it is not, toggle the display between "Clock is running", "Clock is paused"
-
- **/
+        $(this).toggleClass('run')
+    });
+});
