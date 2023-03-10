abd2816eb31d2352202d50f74bd562f3d05c373e
Zeko_Madson <zdmadson@gmail.com>
2023-03-10 10:51:41 -0500

    Add toggle display functionality to add wishlist item header
    Addition of the toggle display functionality to wishlite item header. The category, wishlist item and price inputs then become visible

--- wishlist/script.js
+++ wishlist/script.js
@@ -2,3 +2,9 @@
 Additional Functionality:
     Be able to drag and drop based on priority
  */
+
+$(document).ready(function(){
+    $(".toggle-btn").click(function(){
+        $(this).next(".toggle-item").toggleClass("hidden");
+    });
+});
