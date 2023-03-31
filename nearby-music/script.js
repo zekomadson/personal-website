121276a659a74ae1cdb671beff2b9dbf0506035f
Zeko_Madson <zdmadson@gmail.com>
2023-03-31 16:05:38 -0400

    Add HTML CSS boilerplate code

---
+++ nearby-music/script.js
@@ -0,0 +1,12 @@
+const toggleButtons = document.querySelectorAll(".toggle-btn");
+
+/*
+toggleButtons.forEach(function (button) {
+    /** 
+    button.addEventListener("click", () => {
+        var content = this.nextElementSibling;
+        content.classList.toggle("hidden");
+    });
+  
+});
+  */
